fa44a4e3c43341b5df2fd39d109b9d6fb746c5e6
Samwes13 <sampo.westerholm@gmail.com>
2024-01-23 19:46:41 +0200

    vhanges

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const[numero1, setNumero1]= useState('');
+  const[numero2, setNumero2]= useState('');
+  const[tulokset, setTulokset]= useState('');
+
+  const kasitteleSumma = () => {
+    const summa = parseFloat(numero1) + parseFloat(numero2);
+    setTulokset(summa);
+  }; 
+
+  const kasitteleErotus = () => {
+    const erotus = parseFloat(numero1) - parseFloat(numero2);
+    setTulokset(erotus);
+  }; 
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {tulokset} </Text>
+      <TextInput
+          style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+          keyboardType='number-pad'
+          onChangeText={text => setNumero1(text)}
+      /> 
+      <TextInput
+          style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+          keyboardType='number-pad'
+          onChangeText={text => setNumero2(text)}
+      />
+      <View style={{flexDirection: 'row',
+            alignItems: 'center'}}>    
+        <Button title='+' onPress={kasitteleSumma}/>
+      <View style={{width: 12}}/>
+        <Button title='-' onPress={kasitteleErotus}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
